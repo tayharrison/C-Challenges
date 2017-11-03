45c4051a78338411a2c58f6f7355e3cc2c90fa97
Taylor Harrison <taymkelley@gmail.com>
2017-11-03 13:27:08 -0700

    Conditional Radio Button
    C#, Visual Studio

---
+++ ChallengeSimpleCalculator/ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditionalRadioButton.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>
+        Your Note Taking Preferences</p>
+    <form id="form1" runat="server">
+        <p>
+            <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="NoteGroup" Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="penRadioButton" runat="server" GroupName="NoteGroup" Text="Pen" />
+            <br />
+            <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="NoteGroup" Text="Phone" />
+            <br />
+            <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="NoteGroup" Text="Tablet" />
+        </p>
+        <p>
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        </p>
+        <p>
+            <asp:Image ID="resultImage" runat="server" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            /*
+            if (pencilRadioButton.Checked)
+                resultLabel.Text = "You selected Pencil";
+            else if (penRadioButton.Checked)
+                resultLabel.Text = "You selected Pen";
+            else if (phoneRadioButton.Checked)
+                resultLabel.Text = "You selected Phone";
+            else if (tabletRadioButton.Checked)
+                resultLabel.Text = "You selected Tablet";
+            else
+                resultLabel.Text = "Please select an option";
+            */
+
+            
+            if (pencilRadioButton.Checked)
+                resultImage.ImageUrl = "pencil.png";
+            else if (penRadioButton.Checked)
+                resultImage.ImageUrl = "pen.png";
+            else if (phoneRadioButton.Checked)
+                resultImage.ImageUrl = "phone.png";
+            else if (tabletRadioButton.Checked)
+                resultImage.ImageUrl = "tablet.png";
+            else
+                resultLabel.Text = "Please select an option";
+             
+        }
+    }
+}
