e3e39fd42f68df6647e7212c8007c08480aa976a
Taylor Harrison <taymkelley@gmail.com>
2017-12-15 14:52:05 -0800

    Mega Challenge War
    C# and Visual studio challenge creating the card game War

---
+++ MegaChallengeWar/MegaChallengeWar/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Kind { get; set; }
+
+        public int CardValue()
+        {
+            int value = 0;
+
+            switch (this.Kind)
+            {
+                case "Jack":
+                    value = 11;
+                    break;
+                case "Queen":
+                    value = 12;
+                    break;
+                case "King":
+                    value = 13;
+                    break;
+                case "Ace":
+                    value = 14;
+                    break;
+                default:
+                    value = int.Parse(this.Kind);
+                    break;
+            }
+            return value;
+        }
+
+    }
+}
---
+++ MegaChallengeWar/MegaChallengeWar/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar
+{
+    public class Game
+    {
+        private Player _player1;
+        private Player _player2;
+
+        public Game(string player1Name, string player2Name)
+        {
+            _player1 = new Player() { Name = player1Name };
+            _player2 = new Player() { Name = player2Name };
+        }
+
+        public string Play()
+        {
+            Deck deck = new Deck();
+
+            string result = "<h3>Dealing cards ...</h3>";
+            result += deck.Deal(_player1, _player2);
+
+            result += "<h3>Begin battle ...</h3>";
+            int round = 0;
+            while (_player1.Cards.Count != 0 && _player2.Cards.Count != 0)
+            {
+                Battle battle = new Battle();
+                result += battle.PerformBattle(_player1, _player2);
+
+                round++;
+                if (round > 20)
+                    break;
+            }
+            result += determineWinner();
+            return result;
+        }
+
+        private string determineWinner()
+        {
+            string result = "";
+            if (_player1.Cards.Count > _player2.Cards.Count)
+                result += "<br/><span style='color:red;font-weight:bolder;'>Player 1 wins</span>";
+            else
+                result += "<br/><span style='color:blue;font-weight:bolder;'>Player 2 wins</span>";
+
+            result += "<br/><span style='color:red;font-weight:bolder;'>Player1:" + _player1.Cards.Count + "</span> <br/><span style='color:blue;font-weight:bolder;'>Player2:" + _player2.Cards.Count + "</span>";
+            return result;
+        }
+
+    }
+}
